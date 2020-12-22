f5f28377d86660559321784a765729761c990a59
Alejandro Fuentes Gómez <48445978+alefuegom@users.noreply.github.com>
2020-12-22 17:37:32 +0100

    SP1-Populación1
    Se han populado las tablas de usuario, persona, empresa, cliente, trabajador y administrador.

--- back-end/TFG/src/main/resources/db/h2/data.sql
+++ back-end/TFG/src/main/resources/db/h2/data.sql
@@ -1,2 +1,18 @@
-INSERT INTO usuarios VALUES ('test@db.com', 'test');
-INSERT INTO empresas VALUES ('EMPRESA SA', 'A12345678A', 'DIRECCIÓN', '123456789','SE1234567890123456789012','test@db.com');
+INSERT INTO usuarios VALUES (1, 'test1@db.com', 'test');
+INSERT INTO usuarios VALUES (2, 'test2@db.com', 'test');
+INSERT INTO usuarios VALUES (3, 'test3@db.com', 'test');
+INSERT INTO usuarios VALUES (4, 'test4@db.com', 'test');
+INSERT INTO usuarios VALUES (5, 'test5@db.com', 'test');
+
+
+INSERT INTO empresas VALUES (1, 'A12345678A','SE1234567890123456789012', 'DIRECCIÓN', 'EMPRESA SA', 123456789, 1);
+
+INSERT INTO personas VALUES (1, 'apellidos', '12345678A', 'cliente', 123456789, 2);
+INSERT INTO personas VALUES (2, 'apellidos', '12345678B', 'trabajador',  123456789, 3);
+INSERT INTO personas VALUES (3,  'apellidos', '12345678C', 'administrador', 123456789, 4);
+
+INSERT INTO clientes VALUES(1,'SE1234567890123456789012', 'Dirección', 1);
+
+INSERT INTO trabajadores VALUES(1, 'Trabajo1', 2);
+
+INSERT INTO administradores VALUES(1,3);

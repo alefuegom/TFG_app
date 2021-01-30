8251902c759e853bad0f44891145dba199a9204c
Alejandro Fuentes Gómez <48445978+alefuegom@users.noreply.github.com>
2021-01-30 21:36:29 +0100

    [Refactor] Finalizado implementacion AUTH (CL) Finalizada la implementación Auth para el rol de cliente:  - Añadida las restricciones correspondientes  - Contemplado los posibles errores (duplicidad, no formato, vacios) y la visualización de los mismos
     Falta revisión exhaustiva
     Por hacer: Registro para empresa

--- Workspace/Dedesin/main/models.py
+++ Workspace/Dedesin/main/models.py
@@ -5,45 +5,60 @@ from django.db import models
 
 # Con estos patrones podemos validar los campos dni, cif y la cuenta bancaria
 
-DNI_REGEX = RegexValidator(r'^[0-9]{8}[a-z]{1}$', 'Escribe un DNI correcto.')
+DNI_REGEX = RegexValidator(r'[0-9]{8}[A-Za-z]{1}', 'Escribe un DNI correcto.')
 CIF_REGEX = RegexValidator(r'^[a-zA-Z]{1}\d{7}[a-zA-Z0-9]{1}$', 'Escribe un CIF correcto.')
-CUENTA_BANCARIA_REGEX = RegexValidator(r'^[0-9]{20}$', 'Escribe una cuenta bancaria correcta.')
-NUMBER_REGEX = RegexValidator(r'^[0-9]{9}$', 'Escribe un número de teléfono correcto.')
+CUENTA_BANCARIA_REGEX = RegexValidator(r'^[A-Za-z]{2}[0-9]{22}$', 'Escribe una cuenta bancaria correcta.')
+TELEFONO_REGEX = RegexValidator(r'^[0-9]{9}$', 'Escribe un número de teléfono correcto.')
 CONTRASEÑA_REGEX = RegexValidator(r'^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d]{8,}$', 'Escribe una contraseña con al menos 8 caracteres, al menos una letra y un número')
-
 
 # Create your models here.
 class Usuario(models.Model):
     email = models.EmailField(max_length=254, unique=True)
     contraseña = models.CharField(max_length=254, validators=[CONTRASEÑA_REGEX])
 
+    def __str__(self):
+        return self.email
+
 
 class Empresa(models.Model):
     nombre = models.CharField(max_length=50)
     cif = models.CharField(max_length=10, validators=[CIF_REGEX], unique=True)
     direccion = models.TextField()
-    telefono = models.BigIntegerField(validators = [NUMBER_REGEX], unique=True)
-    cuenta_bancaria = models.CharField(max_length=22,validators=[CUENTA_BANCARIA_REGEX],unique=True)
+    telefono = models.IntegerField(validators = [TELEFONO_REGEX], unique=True)
+    cuenta_bancaria = models.CharField(max_length=22,validators=[CUENTA_BANCARIA_REGEX], blank=True)
     usuario = models.OneToOneField(Usuario, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.usuario.email + "-" + self.cif
 
 
 class Persona(models.Model):
     nombre = models.CharField(max_length=50)
     apellidos = models.CharField(max_length=80)
     dni = models.CharField(max_length=9, validators=[DNI_REGEX],unique=True)
-    telefono = models.BigIntegerField(validators = [NUMBER_REGEX],unique=True)
+    telefono = models.IntegerField(validators = [TELEFONO_REGEX],unique=True)
     usuario = models.OneToOneField(Usuario, on_delete=models.CASCADE)
 
+    def __str__(self):
+        return self.usuario.email + "-" + self.dni
 
 class Cliente(models.Model):
     direccion = models.TextField()
-    cuenta_bancaria = models.CharField(max_length=22 ,validators=[CUENTA_BANCARIA_REGEX],unique=True)
+    cuenta_bancaria = models.CharField(max_length=22 ,validators=[CUENTA_BANCARIA_REGEX],null=True)
     persona = models.OneToOneField(Persona, on_delete=models.CASCADE)
 
+    def __str__(self):
+        return self.persona.usuario.email + "-" + self.persona.dni
 
 class Trabajador(models.Model):
     cualificacion = models.TextField(max_length=254)
+    persona = models.OneToOneField(Persona, on_delete=models.CASCADE, default=None)
 
+
+    def __str__(self):
+        return self.persona.usuario.email + "-" + self.persona.dni
 
 class Administrador(models.Model):
-    administrador = models.OneToOneField(Persona, on_delete=models.CASCADE)
+    persona = models.OneToOneField(Persona, on_delete=models.CASCADE,default=None)
+    def __str__(self):
+        return self.persona.usuario.email + "-" + self.persona.dni
--- Workspace/Dedesin/main/views.py
+++ Workspace/Dedesin/main/views.py
@@ -1,3 +1,5 @@
+import string
+
 from django.shortcuts import render, get_object_or_404
 from .models import *
 
@@ -16,19 +18,25 @@ def inicioSesion(request):
             email = form.cleaned_data['email']
             contraseña = form.cleaned_data['contraseña']
             try:
-                usuario = get_object_or_404(Usuario, email=email)
-                if usuario:
-                    persona = Persona.objects.get(usuario=usuario)
-                    if persona:
-                        cliente = Cliente.objects.get(persona=persona)
-                        print("CLIENTASO")
-                        return render(request, 'home.html', {'ex_msg': 'Se ha logueado perfectamente como cliente'})
-                    else:
+                usuario = Usuario.objects.get(email=email)
+                if usuario.contraseña == contraseña:
+                    try:
                         empresa = Empresa.objects.get(usuario=usuario)
-                        return render(request, 'home.html', {'ex_msg': 'Se ha logueado perfectamente como empresa'})
+                        return render(request, 'inicioSesionCliente.html',
+                                      {'form': form, 'ex_msg': 'Ha iniciado correctamente sesión.'})
+                    except:
+                        persona = Persona.objects.get(usuario=usuario)
+                        cliente = Cliente.objects.get(persona=persona)
+                        if cliente:
+                            return render(request, 'inicioSesionCliente.html',
+                                          {'form': form, 'ex_msg': 'Ha iniciado correctamente sesión.'})
+                else:
+                    return render(request, 'inicioSesionCliente.html ',
+                                  {'form': form, 'er_msg': 'Contraseña incorrecto.'})
             except:
-                return (request, 'inicioSesion.html', {'form': form,
-                                                       'er_msg': 'No se ha encontrado ninguna cuenta asociada o su contraseña es incorrecta'})
+                return render(request, 'inicioSesionCliente.html ',
+                              {'form': form, 'er_msg': 'No existe ningún usuario con el email introducido.'})
+
     form = InicioSesionForm()
     return render(request, 'inicioSesionCliente.html', {'form': form})
 
@@ -43,16 +51,47 @@ def registroCliente(request):
             direccion = form.cleaned_data['direccion']
             telefono = form.cleaned_data['telefono']
             cuenta_bancaria = form.cleaned_data['cuenta_bancaria']
+            if cuenta_bancaria == '':
+                cuenta_bancaria = None
+                print(cuenta_bancaria)
             email = form.cleaned_data['email']
             contraseña = form.cleaned_data['contraseña']
-            print("FUNCIONA")
             usuario = Usuario(email=email, contraseña=contraseña)
-            usuario.save()
+            try:
+                usuario.save()
+            except:
+                return render(request, 'registroCliente.html',
+                              {'er_msg': 'El usuario ya existe en la base de datos', 'form': form})
+
             persona = Persona(nombre=nombre, apellidos=apellidos, dni=dni, telefono=telefono, usuario=usuario)
-            persona.save()
-            cliente = Cliente(direccion=direccion,
-                              cuenta_bancaria=cuenta_bancaria, persona=persona)
-            cliente.save()
-            return render(request, 'home.html', {'ex_msg': 'El registro se ha realizado correctamente.'})
+            try:
+                persona.save()
+            except:
+                usuario.delete()
+                return render(request, 'registroCliente.html',
+                              {'er_msg': 'Ya existe un usuario con el DNI/Telefono introducidos', 'form': form})
+
+            cliente = Cliente(direccion=direccion, cuenta_bancaria=cuenta_bancaria, persona=persona)
+            if not cuenta_bancaria:
+                cliente.save()
+            else:
+                cuentas_bancarias_clientes = []
+                cuentas_bancarias_empresas = []
+                for c in Cliente.objects.all():
+                    cuentas_bancarias_clientes.append(c.cuenta_bancaria)
+                for e in Empresa.objects.all():
+                    cuentas_bancarias_empresas.append(e.cuenta_bancaria)
+
+                if (cuenta_bancaria in cuentas_bancarias_empresas) or (cuenta_bancaria in cuentas_bancarias_clientes):
+                    persona.delete()
+                    usuario.delete()
+                    return render(request, 'registroCliente.html',
+                                  {'er_msg': 'La cuenta bancaria introducida ya existe', 'form': form})
+                else:
+                    cliente.save()
+            return render(request, 'home.html', {'ex_msg': 'El registro se ha realizado correctamente.', 'form': form})
+        else:
+            return render(request, 'registroCliente.html', {'form': form, 'form.errors': form.errors})
+
     form = RegistroClienteForm()
     return render(request, 'registroCliente.html', {'form': form})

35b8f6aa31c660c7ae0add8607e3a1d22953f4a1
Alejandro Fuentes Gómez <48445978+alefuegom@users.noreply.github.com>
2021-02-02 23:54:08 +0100

    [MOD] Interf<z gráfica home Se ha modificado algún defecto que estaba en la Home. Se añadido el apartado de contacto en el footer.

--- Workspace/Dedesin/apps/main/forms.py
+++ Workspace/Dedesin/apps/main/forms.py
@@ -10,26 +10,26 @@ CONTRASEÑA_REGEX = RegexValidator(r'^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d]{8,}$',
 
 
 class RegistroClienteForm(forms.Form):
-    nombre = forms.CharField()
-    apellidos = forms.CharField()
-    dni = forms.CharField(validators=[DNI_REGEX])
-    email = forms.EmailField()
-    contraseña = forms.CharField(widget=forms.PasswordInput, validators=[CONTRASEÑA_REGEX])
-    direccion = forms.CharField()
-    telefono = forms.CharField(validators=[TELEFONO_REGEX])
-    cuenta_bancaria = forms.CharField(validators=[CUENTA_BANCARIA_REGEX], required=False)
+    nombre = forms.CharField(label="Nombre")
+    apellidos = forms.CharField(label="Apellidos")
+    dni = forms.CharField(validators=[DNI_REGEX], label="DNI")
+    email = forms.EmailField(label="E-mail")
+    contraseña = forms.CharField(widget=forms.PasswordInput, validators=[CONTRASEÑA_REGEX], label="Contraseña")
+    direccion = forms.CharField(label="Dirección")
+    telefono = forms.CharField(validators=[TELEFONO_REGEX], label="Teléfono")
+    cuenta_bancaria = forms.CharField(validators=[CUENTA_BANCARIA_REGEX], required=False, label="Cuenta bancaria")
 
 
 class RegistroEmpresaForm(forms.Form):
-    nombre = forms.CharField()
-    CIF = forms.CharField(validators=[CIF_REGEX])
-    email = forms.EmailField()
-    contraseña = forms.CharField(widget=forms.PasswordInput, validators=[CONTRASEÑA_REGEX])
-    direccion = forms.CharField()
-    telefono = forms.CharField(validators=[TELEFONO_REGEX])
-    cuenta_bancaria = forms.CharField(validators=[CUENTA_BANCARIA_REGEX], required=True)
+    nombre = forms.CharField(label="Nombre")
+    CIF = forms.CharField(validators=[CIF_REGEX],label="CIF")
+    email = forms.EmailField(label="E-mail")
+    contraseña = forms.CharField(widget=forms.PasswordInput, validators=[CONTRASEÑA_REGEX], label="Contraseña")
+    direccion = forms.CharField(label="Dirección")
+    telefono = forms.CharField(validators=[TELEFONO_REGEX], label="Teléfono")
+    cuenta_bancaria = forms.CharField(validators=[CUENTA_BANCARIA_REGEX], required=True,label="Cuenta bancaria")
 
 
 class InicioSesionForm(forms.Form):
-    email = forms.EmailField()
-    contraseña = forms.CharField(widget=forms.PasswordInput)
+    email = forms.EmailField(label="E-mail")
+    contraseña = forms.CharField(widget=forms.PasswordInput, label="Contraseña")

fb93b5d5b40847d8ab93b214d014857d6f7f291a
Alejandro Fuentes Gómez <48445978+alefuegom@users.noreply.github.com>
2021-04-11 14:42:50 +0200

    Arreglos para el deploy

--- Dedesin/Dedesin/settings.py
+++ Dedesin/Dedesin/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 """
 
 from pathlib import Path
-
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -59,6 +59,8 @@ MIDDLEWARE = [
 ]
 
 ROOT_URLCONF = 'Dedesin.urls'
+
+STATIC_ROOT= "/Users/alesk/Desktop/Universidad/TFG_app/TFG_app/Dedesin/main/static"
 
 TEMPLATES = [
     {

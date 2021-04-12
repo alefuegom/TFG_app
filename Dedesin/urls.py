809f4ade04e4334bffc93dd31abdaebb25650703
Alejandro_Fuentes_Gómez <alejandro.fuentes@guadaltech.es>
2021-04-12 10:58:12 +0200

    [URL] Deploy

--- Dedesin/urls.py
+++ Dedesin/urls.py
@@ -13,9 +13,11 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from django.conf import settings
 from django.contrib import admin
 from django.urls import path, include
 from main import views
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -26,7 +28,7 @@ urlpatterns = [
     path('administrador/', include('main.administrador.urls'))
 
 
-]
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
 
 handler404 = views.handler404
 

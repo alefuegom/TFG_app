ba67fadeae2f529099632e7ed20aaf343656361b
Alejandro Fuentes Gómez <48445978+alefuegom@users.noreply.github.com>
2021-04-21 21:04:18 +0200

    [IMPL] Filtros para el rol de empresa Se ha implementado todos los filtros en las entidades de solicitudes de servicio y los servicios para el rol de empresa.

--- main/empresa/views.py
+++ main/empresa/views.py
@@ -3,6 +3,7 @@ from django.shortcuts import render, redirect
 from django.core.paginator import Paginator
 from .forms import *
 from ..models import *
+from .filters import *
 from django.contrib.auth import logout as do_logout
 
 
@@ -74,19 +75,23 @@ def logout(request):
 def list_servicios_empresa(request):
     if esEmpresa(request):
         usuario = User.objects.filter(username=request.user.username)[0]
-        solicitudes = SolicitudServicio.objects.filter(usuario=usuario)
-        servicios = []
-        for solicitud in solicitudes:
-            servicio = Servicio.objects.filter(solicitudServicio=solicitud)
-            if servicio:
-                servicios.append(servicio[0])
-        if servicios:
-            paginator = Paginator(servicios, 10)
-            page_number = request.GET.get('page')
-            page_obj = paginator.get_page(page_number)
-            return render(request, 'servicioEmpresa.html', {'page_obj': page_obj, 'num_servicios': len(servicios)})
+        servicios = Servicio.objects.filter(solicitudServicio__usuario=usuario)
+        if len(servicios) > 0:
+            servicios = servicios.order_by('-solicitudServicio__fecha')
+            servicioFilter = ServicioEmpresaFilter(request.GET, queryset=servicios)
+            servicios = servicioFilter.qs
+            if len(servicios) > 0:
+                paginator = Paginator(servicios, 10)
+                page_number = request.GET.get('page')
+                page_obj = paginator.get_page(page_number)
+                return render(request, 'servicioEmpresa.html', {'page_obj': page_obj,
+                                                                'num_servicios': len(servicios),
+                                                                'servicioFilter': servicioFilter})
+            else:
+                msg_error = "No existe ningún servicio con los filtros introducidos."
+                return render(request, 'servicioEmpresa.html', {'msg_error': msg_error})
         else:
-            return render(request, 'servicioEmpresa.html')
+            return render(request, 'servicioCliente.html')
     else:
         return redirect('/errorPermiso/')
 
@@ -112,12 +117,20 @@ def list_solicitud_servicio_empresa(request):
     if esEmpresa(request):
         usuario = User.objects.filter(username=request.user.username)[0]
         solicitudes = SolicitudServicio.objects.filter(usuario=usuario)
-        if solicitudes:
-            paginator = Paginator(solicitudes, 10)
-            page_number = request.GET.get('page')
-            page_obj = paginator.get_page(page_number)
-            return render(request, 'solicitudServicioEmpresa.html', {'page_obj': page_obj,
-                                                                     'num_solicitudes': solicitudes.count()})
+        if len(solicitudes) > 0:
+            solicitudServicioFilter = SolicitudServicioEmpresaFilter(request.GET, queryset=solicitudes)
+            solicitudes = solicitudServicioFilter.qs
+            if len(solicitudes) > 0:
+                solicitudes = solicitudes.order_by('-fecha')
+                paginator = Paginator(solicitudes, 10)
+                page_number = request.GET.get('page')
+                page_obj = paginator.get_page(page_number)
+                return render(request, 'solicitudServicioEmpresa.html', {'page_obj': page_obj,
+                                                                         'num_solicitudes': solicitudes.count(),
+                                                                         'solicitudServicioFilter': solicitudServicioFilter})
+            else:
+                msg_error = "No existe ninguna solicitud de servicio con los filtros introducidos."
+                return render(request, 'solicitudServicioEmpresa.html', {'msg_error': msg_error})
         else:
             return render(request, 'solicitudServicioEmpresa.html')
     else:
